7db3f48ff20ae5caf210d3f986d516e45fbd52fc
psnaut <sghangam123@gmail.com>
2018-11-05 09:35:27 -0500

    Initial project commit

---
+++ mboard/src/main/java/com/homework/mboard/MboardApplication.java
@@ -0,0 +1,12 @@
+package com.homework.mboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MboardApplication.class, args);
+	}
+}
